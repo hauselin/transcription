169cfcdf75043f9f545c126bc8e7f18c666f6a3e
Hause Lin <hauselin@gmail.com>
2024-04-27 23:07:54 -0400

    Use onmount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { onMount } from 'svelte';
+
 	// https://github.com/hyperaudio/hyperaudio-lite
 	// https://hyperaudio.github.io/hyperaudio-lite-editor/
 	const newPlayer = () => {
@@ -11,9 +13,24 @@
 			}
 		});
 	};
+
+	onMount(() => {
+		const script = document.createElement('script');
+		script.src = '/transcription.js';
+		script.async = true;
+		script.onload = () => {
+			newPlayer();
+		};
+
+		document.head.appendChild(script);
+
+		return () => {
+			document.head.removeChild(script);
+		};
+	});
 </script>
 
 <svelte:head>
 	<!-- <script async src="https://svelte-transcription-player.vercel.app"></script> -->
-	<script async src="transcription.js" on:load={newPlayer()}></script>
+	<!-- <script async src="transcription.js" on:load={newPlayer()}></script> -->
 </svelte:head>
