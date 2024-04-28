c3a32ef158940f63fbbf52a3d48f77e9e1e0f3f9
Hause Lin <hauselin@gmail.com>
2024-04-27 21:08:40 -0400

    Play audio and aligned text

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+	<!-- <script async src="https://svelte-transcription-player.vercel.app"></script> -->
+	<script async src="transcription.js"></script>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	// https://github.com/gstefanic/svelte-transcription-player
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		new TranscriptionPlayer({
+			target: document.body,
+			props: {
+				audio: 'demo.mp3',
+				transcription: 'demo.json',
+				playerHeight: 'small'
+			}
+		});
+	});
+</script>
